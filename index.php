746035b9f4a4c640f51ab9e0fe6f25ea1f8c9a2e
a1670291 <a1670291@student.adelaide.edu.au>
2020-11-08 20:52:48 +1030

    adding simple styles

--- index.php
+++ index.php
@@ -1,12 +1,35 @@
 <?php
-    include 'dbh.php';
+include 'dbh.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <style>
+        .button {
+            background-color: #130529;
+            border-radius: 25px;
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+        .comment-section {
+            background-color: #EEEEEE;
+            border-radius: 10px;
+            border: solid;
+        }
+    </style>
+
     <title>Test ground for PHP and SQL with AJAX</title>
     <script src="jquery-3.5.1.js"></script>
     <script>
@@ -17,16 +40,18 @@
                 $("#comments").load("load-comments.php", {
                     newNumComments: numComments,
 
-                }); 
+                });
             });
         });
     </script>
 </head>
+
 <body>
-    <div id="comments">
-        
+    <div id="comments" class=comment-section>
+        <br>
     </div>
 
-    <button id="btn">Click me</button>
+    <button id="btn" class=button>Click me to load comments from database</button>
 </body>
+
 </html>
--- load-comments.php
+++ load-comments.php
@@ -8,12 +8,11 @@
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
             echo "<p>";
-            echo $row['author'];
+            echo "Author: " . $row['author'];
             echo "<br>";
-            echo $row['msg'];
+            echo "Message: " . $row['msg'];
             echo "</p>";
         }
     } else {
         echo "No comments in result";
     }
-?>
